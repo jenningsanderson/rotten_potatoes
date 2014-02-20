0196b236e8abccc14b9a9baaa4de6edab29d0926
Jennings Anderson <jennings.anderson@colorado.edu>
2014-02-19 18:58:09 -0700

    Have it running just like I want it

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,9 +2,34 @@
 class MoviesController < ApplicationController
 
   def index
-    @movies = Movie.all
+    @all_ratings = Movie.pluck(:rating).uniq  #Get all ratings
 
-    @all_ratings = Movie.pluck(:rating).uniq
+    session[:ratings] ||= @all_ratings        #Default to all ratings
+
+    if params.has_key? 'commit' and !params.has_key? 'ratings' #This is the case where all ratings are unclicked
+      params['ratings'] = @all_ratings
+    end
+
+    if params.has_key? 'ratings'              #If params is set, then use that.
+      session[:ratings] = params[:ratings]
+    end
+
+    #Actually call the database
+    @movies = Movie.where(:rating => session[:ratings])
+
+    #Sort movies if there is desire to be sorted
+    session[:sort] ||= 'title'                #Default to sort by title
+    
+    if params.has_key? :sort
+      session[:sort] = params[:sort]
+    end
+
+    #Sort the Movies object
+    @movies = @movies.order(session[:sort])
+
+    #We want to see it too:
+    instance_variable_set("@#{session[:sort]}_header", "hilite")
+
   end
 
   def show
@@ -18,7 +43,6 @@ class MoviesController < ApplicationController
   end
 
   def create
-    debugger
     @movie = Movie.create!(params[:movie])
     flash[:notice] = "#{@movie.title} was successfully created."
     redirect_to movies_path
